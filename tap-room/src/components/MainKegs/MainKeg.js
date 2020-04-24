87a565f85df659613eefe3ecc15a47d47296d7f6
Jeremy Kale Padot <jeremy@padot.us>
2020-04-24 12:08:40 -0700

    add a create new keg form

---
+++ tap-room/src/components/MainKegs/MainKeg.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import Keg from './Keg';
+import PropTypes from "prop-types";
+import { v4 } from 'uuid';
+
+function MainKeg(props){
+
+  function handleNewKegFormSubmit(event) {
+    event.preventDefault();
+    props.onNewKegCreation({
+      image: event.target.kegImg.value, 
+      name: event.target.kegName.value, 
+      brand: event.target.brandName.value, 
+      price: event.target.price.value,
+      alcoholPercent: event.target.alcoholPercent.value,
+      id: v4()});
+  }
+
+  
+  return (
+    <React.Fragment>
+      <div className="kegs">
+        {props.kegs.map((keg, index) => (
+          <Album 
+            key={keg.id}
+            name={keg.name}
+            brand={keg.brand}
+            image={keg.image}
+            price={keg.price}
+            alcoholPercent={keg.alcoholPercent}
+            id={keg.id}
+            onKegClick={props.onKegSelection}
+            handleKegDelete={props.handleKegDelete} />
+        ))}
+      </div>
+      
+      <form className="createForm" onSubmit={handleNewKegFormSubmit}>
+        <h3>Add New Keg</h3>
+        <input
+          type="text"
+          name="kegName"
+          placeholder="Beer Name" />
+        <input
+          type="text"
+          name="brandName"
+          placeholder="Brand Name" />
+        <input
+          type="text"
+          name="kegImg"
+          placeholder="Link to Keg Image" />
+          <input
+          type="text"
+          name="kegPrice"
+          placeholder="How much is it?" />
+          <input
+          type="text"
+          name="alcoholPercent"
+          placeholder="Whats the Alcohol % ?" />
+        <button type='submit'>Add Keg</button>
+      </form>
+    </React.Fragment>
+  )
+}
+
+MainKeg.propTypes = {
+  kegs: PropTypes.arrayOf(PropTypes.object),
+  onNewKegCreation: PropTypes.func,
+  onKegSelection: PropTypes.func
+}
+
+export default MainKeg;
