f8ae1ac3963f512a328ff25f60ae145024eb225f
Jeremy Kale Padot <jeremy@padot.us>
2020-04-24 11:43:11 -0700

    add purchace,  restock to app.js

--- tap-room/src/App.js
+++ tap-room/src/App.js
@@ -18,7 +18,7 @@ constructor(props) {
         price: "$4.20",
         alcoholPercent: "8%",
         inventory: 124,
-        id: ""
+        id: "666"
       },
        {
         name: "Your Dog Sucks Milk Stout",
@@ -27,8 +27,8 @@ constructor(props) {
         price: "$4.20",
         alcoholPercent: "8%",
         inventory: 124,
-        id: ""
-      },
+        id: "667"
+      } //you might need to put a comma here if you dont open up that other beer
       //   {
       //   name: "Your Dog Sucks Milk Stout",
       //   brand: "Princess Haru's Brewery",
@@ -39,10 +39,52 @@ constructor(props) {
       //   id: ""
       // },
 
-    ]
+    ],
+    currentSelectedKeg: {},
   }
 }
+handleKegSelection = (id) => {
+  const selectedKeg = this.state.kegList.filter(keg => keg.id === id)[0];
+  this.setState({
+    currentSelectedKeg: selectedKeg,
+    showHomePage: False
+  })
+}
 
+handleAddingNewKeg = (newKeg) => {
+  const newKeglist = this.state.kegList.concat(newKeg);
+  this.setState({kegList: newKegList})
+}
+
+handleBackToAlbums = () => {
+  this.setState({
+    showHomePage: true
+  })
+}
+
+handleKegPurchase = (id) => {
+    const selectedKeg = this.state.kegList.filter(keg => keg.id === id)[0];
+    // const newShoppingCartItems = this.state.shoppingCartItems.concat(selectedAlbum);
+    const newInventory = selectedKeg.inventory -1;
+    const updatedKeg = {...selectedKeg, inventory: newInventory};
+    const oldKegs = this.state.kegList.filter(keg => keg.id !== id);
+    this.setState({
+      // shoppingCartItems: newShoppingCartItems,
+      kegList: [...oldKegs, updatedKeg],
+      currentSelectedKeg: updatedKeg
+    });
+}
+
+  handleKegRestock = (id) => {
+    const selectedKeg = this.state.kegList.filter(keg => keg.id === id)[0];
+    const newInventory = selectedKeg.inventory +124;
+    const updatedKeg = {...selectedKeg, inventory: newInventory}
+    const oldKegs = this.state.kegList.filter(keg => keg.id !== id);
+    this.setState({
+      kegList: [...oldKegs, updatedKeg],
+      currentSelectedKeg: updatedKeg
+    });
+  }
 
 
 
