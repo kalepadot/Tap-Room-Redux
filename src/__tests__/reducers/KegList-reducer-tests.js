05729e95237c8a37565c0e6ea1baba17045bbbe9
Jeremy Kale Padot <jeremy@padot.us>
2020-05-01 09:46:04 -0700

    fail, kegdata undefined, wrong fail message

---
+++ src/__tests__/reducers/KegList-reducer-tests.js
@@ -0,0 +1,40 @@
+import KegListReducer from '../../reducers/KegList-reducer';
+
+describe('KegListReducer', () => {
+
+let action;
+const kegData = {
+  name: "I'd Rather Be Smokin Cat Nip Pale Ale",
+  brand: "Princess Haru's Brewery",
+  // image: BeerImage,
+  price: "$4.20",
+  alcoholPercent: "8%",
+  inventory: 124,
+  id: "666"
+};
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(KegListReducer({}, {type: null})).toEqual({});
+  });
+});
+
+  test('Should successfully add new keg data to MasterKegList', () => {
+    const { name, brand, price, alcoholPercent, inventory, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      inventory: inventory,
+      id: id
+    };
+    
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        inventory: inventory,
+        id: id
+      }
+    });
+  });
