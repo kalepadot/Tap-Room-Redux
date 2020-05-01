b4915b5ccbd5481604e40ddc63297b28a61eb849
Jeremy Kale Padot <jeremy@padot.us>
2020-05-01 10:58:14 -0700

    last commit for this repo, getting /app.test error that cant be resolved, starting fresh

--- src/__tests__/reducers/KegList-reducer.test.js
+++ src/__tests__/reducers/KegList-reducer.test.js
@@ -1,4 +1,5 @@
 import KegListReducer from '../../reducers/KegList-reducer';
+import * as c from './../../actions/ActionTypes';
 
 describe('KegListReducer', () => {
 
@@ -13,21 +14,32 @@ const kegData = {
   inventory: 124,
   id: 666
 };
+
+const currentState = {
+  1: {name: "I'd Rather Be Smokin Cat Nip Pale Ale",
+  brand: "Princess Harus's Brewery",
+  price: "$4.20",
+  alcoholPercent: "8%",
+  inventory: 124, 
+  id: 666 }
+};
+
   test('Should return default state if there is no action type passed into the reducer', () => {
-    expect(KegListReducer({}, {type: null})).toEqual({});
+    expect(KegListReducer({}, { type: null })).toEqual({});
   });
 
 
   test('Should successfully add new keg data to MasterKegList', () => {
     const { name, brand, price, alcoholPercent, inventory, id } = kegData;
     action = {
-      type: 'ADD_KEG',
+      type: c.ADD_KEG,
       name: name,
       brand: brand,
       price: price,
+      alcoholPercent: alcoholPercent,
       inventory: inventory,
       id: id
-    }
+    };
     
     expect(KegListReducer({}, action)).toEqual({
       [id] : {
